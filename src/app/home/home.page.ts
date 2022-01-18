6f0a9c01862c4592347495c6e200bc24e3f0f9c4
Moira Leon <moira.leon2@gmail.com>
2022-01-18 00:18:33 -0700

    adding pop up

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+ 
+  taskName: any = ''; // Entered Text
+  taskList = []; // Array to store tasks
+  completedTasks = []; //array to store completed tasks
+   
   constructor() {}
 
+  addTask() {
+  if (this.taskName.length > 0) {
+  let task = this.taskName;
+  this.taskList.push(task);
+  console.log(task);//for testing
+  this.taskName = '';
+  }
+  }
+
+//not actually storing information in an array or consoling completedTasks array
+markTaskCompleted(index) {
+  let task = this.taskName;
+  this.completedTasks.push(task);
+  console.log((this.completedTasks[0]).toString); //for testing
+  this.taskList.splice(index, 1);
 }
+
+deleteTask(index) {
+  this.taskList.splice(index, 1);
+  }
+
+}
+
